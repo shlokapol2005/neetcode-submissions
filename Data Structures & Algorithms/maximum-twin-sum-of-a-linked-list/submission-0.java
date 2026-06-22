4516565e8e611fc185afbfe81f175eda76ea4191
Shloka Pol <140430581+shlokapol2005@users.noreply.github.com>
2026-06-22 22:35:09 +0530

    Add: maximum-twin-sum-of-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-0.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        
+        //regular slow and fast approach 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+}
+//jaha slow hai thats middle, 
+ // Reverse second half
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // Calculate maximum twin sum
+        int maxSum = 0;
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+            maxSum = Math.max(maxSum, first.val + second.val);
+            first = first.next;
+            second = second.next;
+        }
+
+        return maxSum;
+
+    }
+}
