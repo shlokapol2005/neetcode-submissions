b0e38a6689850056f05c579e0aa775964829d32a
Shloka Pol <140430581+shlokapol2005@users.noreply.github.com>
2026-06-24 00:25:11 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {  // nge approach normal
+    
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() &&
+                   temperatures[st.peek()] <= temperatures[i]) {
+                st.pop();
+            }
+
+            if (!st.isEmpty()) {
+                ans[i] = st.peek() - i;
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+}
