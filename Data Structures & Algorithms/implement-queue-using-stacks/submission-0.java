3a3612aea3377e0cac1943ed5a8e4605690d3fcc
Shloka Pol <140430581+shlokapol2005@users.noreply.github.com>
2026-06-23 11:45:49 +0530

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.java
@@ -0,0 +1,38 @@
+
+class MyQueue {
+
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    // two stacks s1 and s2
+
+    public void push(int x) {
+        s1.push(x);   // use stack push
+    }
+    
+    public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+    
+    public int peek() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
